e1abf01eb61a263b9d7b288a5bf587f63d38db62
DiogoNMSantos <diogo.nogal.santos@gamil.com>
2022-09-07 23:14:33 +0200

    remove duplication from fizzBuzz

--- src/FizzBuzz/FizzBuzz.ts
+++ src/FizzBuzz/FizzBuzz.ts
@@ -1,13 +1,5 @@
 const fizzBuzz = (input: number): string => {
-  if (input === 15) {
-    return 'FizzBuzz';
-  }
-
-  if (input === 30) {
-    return 'FizzBuzz';
-  }
-
-  if (input === 45) {
+  if (input % 3 === 0 && input % 5 === 0) {
     return 'FizzBuzz';
   }
 
