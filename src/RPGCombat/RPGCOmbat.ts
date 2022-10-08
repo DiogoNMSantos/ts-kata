6a9978779b6146d0c4c57f7be0ac272d0d4af9bc
Diogo Nogal Santos <diogo.nogal.santos@gmail.comm>
2022-10-08 13:37:59 +0200

    feat: starts alive at lvl 1 with 1000 health

--- src/RPGCombat/RPGCOmbat.ts
+++ src/RPGCombat/RPGCOmbat.ts
@@ -5,6 +5,9 @@ class Character {
   lvl() {
     return 1;
   }
+  health() {
+    return 1000;
+  }
 }
 
 export default Character;
--- src/test/RPGCombat/RPGCombat.test.ts
+++ src/test/RPGCombat/RPGCombat.test.ts
@@ -1,9 +1,10 @@
 import Character from '../../RPGCombat/RPGCOmbat';
 
 describe('RPG Combat', () => {
-  test('A character starts alive at lvl 1', () => {
+  test('A character starts alive at lvl 1 with 1000 health', () => {
     const character: Character = new Character();
     expect(character.isAlive()).toBe(true);
+    expect(character.health()).toBe(1000);
     expect(character.lvl()).toBe(1);
   });
 });
