323b7a8bce73b8b342789c09c18f4e9c96610fe1
Diogo Nogal Santos <diogo.nogal.santos@gmail.comm>
2022-10-11 19:58:47 +0200

    refactor: remove duplication in tests

--- src/test/RPGKata/RPGKata.test.ts
+++ src/test/RPGKata/RPGKata.test.ts
@@ -1,18 +1,18 @@
 import Character from '../../RPGKata/RPGKata';
 
 describe('RPG Kata', () => {
-  test('Melee characters, when created, have health starting at 1000', () => {
+  describe('Melee characters', () => {
     const melee = new Character();
-    expect(melee.health()).toBe(1000);
-  });
+    test('when created, have health starting at 1000', () => {
+      expect(melee.health()).toBe(1000);
+    });
 
-  test('Melee characters start at level 1', () => {
-    const melee = new Character();
-    expect(melee.level()).toBe(1);
-  });
+    test('start at level 1', () => {
+      expect(melee.level()).toBe(1);
+    });
 
-  test('Melee characters start alive', () => {
-    const melee = new Character();
-    expect(melee.alive()).toBe(true);
+    test('start alive', () => {
+      expect(melee.alive()).toBe(true);
+    });
   });
 });
