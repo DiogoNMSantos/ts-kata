7c015659fcd74a35a8d9e543583e4f7b00364cec
Diogo Nogal Santos <diogo.nogal.santos@gmail.comm>
2022-11-12 17:55:51 +0100

    Refactor: Melee attack

--- src/RPGKata/RPGKata.ts
+++ src/RPGKata/RPGKata.ts
@@ -16,6 +16,8 @@ abstract class Character {
 
   private readonly DefaultHealing = 80;
 
+  private readonly MinimumHealth = 0;
+
   constructor(maxHealth: number, lvl: number, x = 0) {
     this.maxHealth = maxHealth;
     this.currentHealth = maxHealth;
@@ -32,7 +34,7 @@ abstract class Character {
   }
 
   alive(): boolean {
-    return this.currentHealth > 0;
+    return this.currentHealth > this.MinimumHealth;
   }
 
   attack(defender: Character): void {
@@ -77,8 +79,8 @@ abstract class Character {
   }
 
   protected checkDefenderDied(defender: Character): void {
-    if (defender.currentHealth <= 0) {
-      defender.currentHealth = 0;
+    if (defender.currentHealth <= this.MinimumHealth) {
+      defender.currentHealth = this.MinimumHealth;
     }
   }
 
@@ -98,24 +100,46 @@ class MeleeCharacter extends Character {
     super(1000, lvl, x);
   }
 
+  private readonly MaxMeleeRange = 2;
+
+  private readonly MeleeLevelThreshold = 5;
+
+  private readonly DecreasedMeleeDamage = 40;
+
+  private readonly IncreasedMeleeDamage = 120;
+
+  private readonly DefaultMeleeDamage = 80;
+
   override attack(defender: Character): void {
     if (this.isDefenderItself(defender)) {
       return;
     }
 
-    if (Math.abs(this.position - defender.position) > 2) {
+    if (this.IsNotInMeleeRange(defender)) {
       return;
     }
 
-    if (defender.level() - this.level() >= 5) {
-      defender.currentHealth -= 40;
-    } else if (this.level() - defender.level() >= 5) {
-      defender.currentHealth -= 120;
+    if (this.isBelowMeleeLevelThreshold(defender)) {
+      defender.currentHealth -= this.DecreasedMeleeDamage;
+    } else if (this.IsAboveMeleeLevelThreshold(defender)) {
+      defender.currentHealth -= this.IncreasedMeleeDamage;
     } else {
-      defender.currentHealth -= 80;
+      defender.currentHealth -= this.DefaultMeleeDamage;
     }
 
     this.checkDefenderDied(defender);
+  }
+
+  private IsAboveMeleeLevelThreshold(defender: Character) {
+    return this.level() - defender.level() >= this.MeleeLevelThreshold;
+  }
+
+  private isBelowMeleeLevelThreshold(defender: Character) {
+    return defender.level() - this.level() >= this.MeleeLevelThreshold;
+  }
+
+  private IsNotInMeleeRange(defender: Character) {
+    return Math.abs(this.position - defender.position) > this.MaxMeleeRange;
   }
 }
 
